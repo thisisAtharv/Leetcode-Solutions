673c47e59096493f7c10e18f0fda7d64d7509ecf
Atharv <175135608+thisisAtharv@users.noreply.github.com>
2025-08-30 01:04:17 +0530

    Time: 9 ms (26.21%), Space: 41.5 MB (47.83%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        String prev = "1";
+        for(int i=2;i<=n;i++){
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for(int j=1;j<=prev.length();j++){
+                if(j<prev.length() && prev.charAt(j)==prev.charAt(j-1)){
+                    count++;
+                }
+                else{
+                    sb.append(count).append(prev.charAt(j-1));
+                    count=1;
+                }
+            }
+            prev = sb.toString();
+        }
+        
+        return prev;
+    }
+}
