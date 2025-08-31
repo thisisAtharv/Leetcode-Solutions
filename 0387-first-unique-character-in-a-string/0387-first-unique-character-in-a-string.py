16f5b24e876ed9cfdf64d407603a082b0ddaea8e
Atharv <175135608+thisisAtharv@users.noreply.github.com>
2025-08-31 11:19:37 +0530

    Time: 75 ms (28.32%), Space: 18.3 MB (5.88%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -10,7 +10,7 @@ class Solution:
                 count[x]=1
                 order.append(x)
                 index[x]=i
-        for x in order:
+        for x in s:
             if count[x]==1:
                 return index[x]
         return -1
