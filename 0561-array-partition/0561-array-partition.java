902f633fc1829094f16f02130623726b89fe52f6
Atharv <175135608+thisisAtharv@users.noreply.github.com>
2025-09-05 00:58:32 +0530

    Time: 12 ms (97.41%), Space: 46.7 MB (48.03%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -2,10 +2,8 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int res=0;
-        for(int i=0;i<nums.length;i++){
-            if(i%2==0){
-                res+=nums[i];
-            }
+        for(int i=0;i<nums.length;i+=2){
+            res+=nums[i];
         }
         return res;
     }
