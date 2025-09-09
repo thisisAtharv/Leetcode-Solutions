76747c319e2940b661253fa2cee1afd9391eff83
Atharv <175135608+thisisAtharv@users.noreply.github.com>
2025-09-10 01:15:49 +0530

    Time: 1107 ms (65.85%), Space: 55.2 MB (11.66%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
@@ -3,14 +3,11 @@ class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         def gain(p,t):
             return (p+1)/(t+1) - p/t
-        
         maxheap=[(-gain(p,t),p,t)for p,t in classes]
         heapq.heapify(maxheap)
-
         for _ in range(extraStudents):
             g,p,t = heapq.heappop(maxheap)
             p,t = p+1,t+1
             heapq.heappush(maxheap,(-gain(p,t),p,t))
-        
         total = sum(p/t for _,p,t in maxheap)
         return total/len(classes)
