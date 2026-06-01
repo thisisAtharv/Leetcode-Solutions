ba9d362ea60c256237db4cb5d6536d524d37a539
Atharv <175135608+thisisAtharv@users.noreply.github.com>
2026-06-01 14:56:29 +0530

    Time: 8 ms (43.41%), Space: 42.1 MB (50.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,8 @@
 class Solution {
     public int fib(int n) {
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        int last = fib(n-1);
+        int secondlast = fib(n-2);
+        return  last+secondlast;
     }
 }
